594ce31222ede39940ce6461063578cc58a29be5
Gvxng <yanisachamou@gmail.com>
2026-04-13 01:04:31 -0400

    docker

--- api-gateway-service/src/main/java/com/example/restaurantreservation/RestaurantReservationApplication.java
+++ api-gateway-service/src/main/java/com/example/restaurantreservation/RestaurantReservationApplication.java
@@ -3,7 +3,7 @@ package com.example.restaurantreservation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.example.restaurantreservation.apigateway")
+@SpringBootApplication
 public class RestaurantReservationApplication {
 
     public static void main(String[] args) {
